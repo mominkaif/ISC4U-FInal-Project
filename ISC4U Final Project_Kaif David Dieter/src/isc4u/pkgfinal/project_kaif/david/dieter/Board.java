6299a92d92ecb9dcaa2d74421c3d251710a17e23
dieterwhitt <dieter.whitt@gmail.com>
2022-05-26 13:44:54 -0400

    started Board class

---
+++ ISC4U Final Project_Kaif David Dieter/src/isc4u/pkgfinal/project_kaif/david/dieter/Board.java
@@ -0,0 +1,32 @@
+/* Board class
+ */
+package isc4u.pkgfinal.project_kaif.david.dieter;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dieter
+ */
+public class Board {
+    private Tile[][] tilemap;
+    private Sound soundtrack;
+    private ArrayList<entity> entityList;
+    private static final int width = 10;
+    private static final int height = 15;
+    
+    public Board(){
+        tilemap = new Tile[width][height];
+        sountrack = null;
+        entityList = new ArrayList();
+    }
+    
+    public Board(Tile[][] tilemap, Sound soundtrack, ArrayList entityList){
+        this();
+        this.tilemap = tilemap;
+        this.soundtrack = soundtrack;
+        this.entityList = entityList;
+    }
+    
+    
+}
