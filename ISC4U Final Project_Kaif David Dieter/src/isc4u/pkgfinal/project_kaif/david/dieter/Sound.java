9d9ea0459fa8e8d9d1f9c19b5bf3abfd3c734091
dieterwhitt <dieter.whitt@gmail.com>
2022-05-31 13:41:21 -0400

    commented sound class

--- ISC4U Final Project_Kaif David Dieter/src/isc4u/pkgfinal/project_kaif/david/dieter/Sound.java
+++ ISC4U Final Project_Kaif David Dieter/src/isc4u/pkgfinal/project_kaif/david/dieter/Sound.java
@@ -16,47 +16,74 @@ public class Sound {
     private File file;
     private boolean loop;
     private Clip clip;
-    
+    /**
+     * sound constructor
+     * no parameters
+     */
     public Sound() {
         file = null;
         loop = false;
     }
-    
+    /**
+     * secondary constructor
+     * @param file - sound file being played (.wav)
+     * @param loop - boolean whether or not to loop
+     */
     public Sound(File file, boolean loop) {
         this.file = file;
         this.loop = loop;
     }
-    
+    /**
+     * accessor for the file
+     * @return - sound file
+     */
     public File getFile() {
         return file;
     }
-    
+    /**
+     * accessor for the loop status
+     * @return - boolean loop
+     */
     public boolean getLoop() {
         return loop;
     }
-    
+    /**
+     * mutator for the file
+     * @param f - new file
+     */
     public void setFile(File f) {
         file = f;
     }
-    
+    /**
+     * mutator for the loop boolean
+     * @param l - new loop
+     */
     public void setLoop(boolean l) {
         loop = l;
     }
-    
+    /**
+     * play method
+     * plays the sound
+     * if looping, it will loop
+     */
     public void play() {
         try {
             AudioInputStream ais = AudioSystem.getAudioInputStream(file);
             clip = AudioSystem.getClip();
             clip.open(ais);
             clip.start();
+            System.out.println("playing clip "+file);
             if (loop) {
                 clip.loop(clip.LOOP_CONTINUOUSLY);
             }
         } catch (Exception e) {
-            
+            System.out.println(e);
         }
     }
-    
+    /**
+     * stop method
+     * stops playing the sound
+     */
     public void stop() {
         clip.stop();
     }
