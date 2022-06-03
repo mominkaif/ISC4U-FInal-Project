eea970c6de7446f35459f35700faf5634df72f3b
dieterwhitt <dieter.whitt@gmail.com>
2022-06-03 13:22:58 -0400

    finished enemy class

--- ISC4U Final Project_Kaif David Dieter/src/isc4u/pkgfinal/project_kaif/david/dieter/Enemy.java
+++ ISC4U Final Project_Kaif David Dieter/src/isc4u/pkgfinal/project_kaif/david/dieter/Enemy.java
@@ -9,15 +9,29 @@ import java.awt.Image;
  * @author Dieter
  */
 public class Enemy extends Entity{
-    
+    /**
+     * primary constructor
+     * uses superclass
+     */
     public Enemy(){
         super();
     }
-    
+    /**
+     * secondary constructor
+     * @param imageID - image number
+     * @param x - x location
+     * @param y - y location
+     * @param xSpeed - xspeed
+     * @param ySpeed - yspeed
+     */
     public Enemy(Image image, int x, int y, int xSpeed, int ySpeed){
         super(image, x, y, xSpeed, ySpeed);
     }
-    
+    /**
+     * moves the entity
+     * updates its x and y position based on its speed
+     * if it reaches the edge it respawns on the other side of the map
+     */
     public void move(){
         if(x>=0){
             x+=xSpeed;
@@ -48,18 +62,32 @@ public class Enemy extends Entity{
         }      
     }
     
-    
+    /**
+     * equals method checks if two enemies are the same
+     * they are the same if their x, y and speeds are the same
+     * @param other - entity being checked
+     * @return - whether they are the same
+     */
     public boolean equals(Entity other){
         return x == (other).getXPos() ||
                 y == (other).getYPos() || xSpeed == (other).getXSpeed()
                 || ySpeed == (other).getYSpeed();
     }
-    
+    /**
+     * clone method returns an enemy which is a copy of the object
+     * @return - cloned enemy
+     */
     public Entity clone(){
-        return null;
+        Entity e2;
+        e2 = new Enemy(image, x, y, xSpeed, ySpeed);
+        return e2;
     }
-    
+    /**
+     * toString method returns string listing the enemy's attributes
+     * @return - the string
+     */
     public String toString(){
-        return null;
+        return "\nX Position: " + x + "\nY Position: " + y + "\nX Speed: " + 
+                xSpeed + "\nY Speed: " + ySpeed;
     }
 }
